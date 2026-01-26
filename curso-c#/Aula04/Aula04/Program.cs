310c9fa91ab21cf9c1737b2c5876cb095f18b1eb
Lucas Mota <lucas.barros@unifesspa.edu.br>
2026-01-25 21:54:45 -0300

    Adicionando aula04

---
+++ curso-c#/Aula04/Aula04/Program.cs
@@ -0,0 +1,42 @@
+﻿namespace Program;
+
+public class Program
+{
+    public static void Main()
+    {
+        Console.WriteLine("=============== TELA DE CADASTRO ===============");
+        Console.WriteLine();
+
+        Console.Write("Nome completo: ");
+        string nomeCompleto = Console.ReadLine()!;
+
+        Console.Write("Nome de usuário: ");
+        string nomeUsuario = Console.ReadLine()!;
+
+        Console.Write("Idade: ");
+        int idade = Convert.ToInt32(Console.ReadLine()!);
+
+        Console.Write("E-mail: ");
+        string email = Console.ReadLine()!;
+
+        Console.Write("Endereço: ");
+        string endereco = Console.ReadLine()!;
+
+        Console.Write("Estado civil (Solteiro(a), Casado(a), Divorciado(a), Viúvo(a)): ");
+        string estadoCivil = Console.ReadLine()!;
+
+        Console.WriteLine();
+        Console.WriteLine("=============== CADASTRO REALIZADO ===============");
+        Console.WriteLine();
+
+        Console.WriteLine("Usuário: " + nomeUsuario);
+        Console.WriteLine("Nome completo: " + nomeCompleto);
+        Console.WriteLine("Idade: " + idade + " anos");
+        Console.WriteLine("E-mail: " + email);
+        Console.WriteLine("Endereço: " + endereco);
+        Console.WriteLine("Estado civil: " + estadoCivil);
+
+        Console.WriteLine();
+        Console.WriteLine("===============================================");
+    }
+}
