8844170ce88ff293b8b1021421f8001ec7de448a
Lucas Mota <lucas.barros@unifesspa.edu.br>
2026-01-26 21:43:42 -0300

    Adicionando atividade Aula05

--- curso-c#/Aula05/Aula05/Program.cs
+++ curso-c#/Aula05/Aula05/Program.cs
@@ -1,2 +1,35 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿namespace Aula05;
+
+public class Program
+{
+    public static void Main()
+    {
+        Console.WriteLine("============= Calculadora ============= \n");
+
+        Console.Write("Digite o primeiro numero: ");
+        int number1 = Convert.ToInt32(Console.ReadLine());
+
+        Console.Write("Digite o segundo numero:");
+        int number2 = Convert.ToInt32(Console.ReadLine());
+
+        Console.WriteLine("");
+
+        int soma = number1 + number2;
+        int subtracao = number1 - number2;
+        int multiplicacao = number1 * number2;
+        int divisao = number1 / number2;
+        int modulo = number1 % number2;
+
+        Console.WriteLine("============= Resultados ============== \n");
+
+        Console.WriteLine("A soma do numero 1 com o numero 2 é:" + soma);
+        Console.WriteLine("A subtraçao do numero 1 com o numero 2 é: " + subtracao);
+        Console.WriteLine("A multiplicação do numero 1 com o numero 2 é: " + multiplicacao);
+        Console.WriteLine("A divisão do numero 1 com o numero 2 é: " + divisao);
+        Console.WriteLine("O modulo do numero 1 com o numero 2 é: " + modulo);
+
+        Console.WriteLine("");
+        
+        Console.WriteLine("=======================================");
+    }
+}
