20f6255885b940958410c04742e114510fcc06f4
Lucas Mota <lucas.barros@unifesspa.edu.br>
2026-01-27 16:02:39 -0300

    Adicionando a Aula06

---
+++ curso-c#/Aula06/Aula06/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace Aula06;
+
+public class Program
+{
+    public static void Main()
+    {
+        Console.Write("Digite um número: ");
+        int numero = Convert.ToInt32(Console.ReadLine());
+
+        int resultado = numero % 2;
+
+        if (resultado == 0)
+        {
+            Console.WriteLine("O número é par!");
+        }
+        else
+        {
+            Console.WriteLine("O número é ímpar!");
+        }
+    }
+}
